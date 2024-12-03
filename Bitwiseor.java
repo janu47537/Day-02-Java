d4e9103cb3966f0a3cccdba6d7f491cc4cec2ae9
janu47537 <janu47537@gmail.com>
2024-12-03 15:24:20 +0530

    Add files via upload

---
+++ Bitwiseor.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Bitwiseor{
+    public static void main(String[]args){
+        System.out.println(11|18);
+        System.out.println(22|144);
+        System.out.println(88|122);
+        System.out.println(66|164);
+    }
+}
