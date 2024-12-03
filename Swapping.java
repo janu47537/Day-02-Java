1e0167020cff18791452aed5f99202a0096fba8a
janu47537 <janu47537@gmail.com>
2024-12-03 11:59:12 +0530

    Add files via upload

---
+++ Swapping.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Swapping
+{
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int b=sc.nextInt();
+        int g=sc.nextInt();
+        int c=b;
+        b=g;
+        g=c;
+        String res=String.format("After swapping\nboy=%d\ngirl=%d",b,g);
+        System.out.println(res);
+    }
+}
